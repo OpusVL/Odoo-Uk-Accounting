7a91ce326cf168321a0dfe8445245f6e781875a5
Armand Cela <armand.cela@opusvl.com>
2020-01-02 10:27:12 +0000

    [O2311] Removing the code in main.py

--- uk_accounting/controllers/main.py
+++ uk_accounting/controllers/main.py
@@ -6,9 +6,3 @@ from odoo import http, exceptions
 
 _logger = logging.getLogger(__name__)
 
-
-class Authorize(http.Controller):
-    
-    @http.route('/auth-redirect', type='http', methods=['GET'])
-    def get_epdq_response(self, **args):
-        return True
