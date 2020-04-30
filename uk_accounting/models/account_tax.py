a795af3e60267515bca7be656dd74897d2c5f6eb
Armand Cela <armand.cela@opusvl.com>
2020-04-30 12:30:42 +0100

    [O2489] tax = 0 when sell price < purchase price

--- uk_accounting/models/account_tax.py
+++ uk_accounting/models/account_tax.py
@@ -30,7 +30,7 @@ class AccountTaxMargin(models.Model):
                 if product:
                     safe_eval('result = (base_amount - product.standard_price ) * {}'.format(
                         self.amount/100.0), localdict, mode="exec", nocopy=True)
-                    return localdict['result']
+                    return localdict['result'] if localdict['result'] > 0 else 0
 
                 else:
                     res = super(AccountTaxMargin, self)._compute_amount(base_amount, price_unit, quantity, product, partner) or 0.0
@@ -40,7 +40,7 @@ class AccountTaxMargin(models.Model):
                         'result = (base_amount - product.standard_price) - (base_amount - product.standard_price ) / (1 + {})'.format(
                             self.amount / 100.0), localdict, mode="exec",
                         nocopy=True)
-                    return localdict['result']
+                    return localdict['result'] if localdict['result'] > 0 else 0
                 else:
                     res = super(AccountTaxMargin, self)._compute_amount(base_amount, price_unit, quantity, product, partner) or 0.0
         else:
@@ -55,10 +55,27 @@ class AccountTaxMargin(models.Model):
             product = product.product_variant_id
         for tax in self.filtered(lambda r: r.amount_type == 'margin'):
             localdict = self._context.get('tax_computation_context', {})
-            localdict.update({'price_unit': price_unit, 'quantity': quantity, 'product': product,
+            localdict.update({'price_unit': price_unit, 'quantity': quantity,
+                              'product': product,
                               'partner': partner, 'company': company})
             safe_eval('result = True', localdict, mode="exec", nocopy=True)
             if localdict.get('result', False):
                 taxes += tax
-        return super(AccountTaxMargin, taxes).compute_all(price_unit, currency, quantity, product, partner,
-                                                          is_refund=is_refund, handle_price_include=handle_price_include)
+        res = super(AccountTaxMargin, taxes).compute_all(
+            price_unit, currency, quantity, product, partner,
+            is_refund=is_refund, handle_price_include=handle_price_include)
+        total_res = {}
+        total_res['base_tags'] = res['base_tags']
+        total_res['taxes'] = []
+        total_base = 0
+        total_tax = 0
+        for lst_taxes in res['taxes']:
+            if lst_taxes.get('price_include'):
+                lst_taxes.update({'base': price_unit * quantity - lst_taxes['amount']})
+            total_res['taxes'].append(lst_taxes)
+            total_base += lst_taxes['base']
+            total_tax += lst_taxes['amount']
+        total_res['total_included'] = total_base + total_tax
+        total_res['total_excluded'] = total_base
+        total_res['total_void'] = total_base + total_tax
+        return total_res
