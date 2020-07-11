86064cc289e2db2427f5b19bd1eb3afeadd0656e
Peter Alabaster <peter.alabaster@opusvl.com>
2020-07-11 09:29:10 +0100

    [O2541] Change om_account_budget field strings

--- om_account_budget/models/account_budget.py
+++ om_account_budget/models/account_budget.py
@@ -95,15 +95,15 @@ class CrossoveredBudgetLines(models.Model):
     paid_date = fields.Date('Paid Date')
     currency_id = fields.Many2one('res.currency', related='company_id.currency_id', readonly=True)
     planned_amount = fields.Monetary(
-        'Planned Amount', required=True,
+        'Budget', required=True,
         help="Amount you plan to earn/spend. Record a positive amount if it is a revenue and a negative amount if it is a cost.")
     computed_practical_amount = fields.Monetary(
         compute='_compute_practical_amount',
         string='Computed Practical Amount',
         help="Amount really earned/spent.")
-    practical_amount = fields.Monetary(string='Practical Amount',
+    practical_amount = fields.Monetary(string='Actual',
                                        help="Amount really earned/spent.")
-    difference_amount = fields.Monetary(string='Difference', )
+    difference_amount = fields.Monetary(string='Variance', )
     company_id = fields.Many2one(related='crossovered_budget_id.company_id', comodel_name='res.company',
         string='Company', store=True, readonly=True)
     crossovered_budget_state = fields.Selection(related='crossovered_budget_id.state', string='Budget State', store=True, readonly=True)
