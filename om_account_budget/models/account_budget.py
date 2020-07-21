4936f90733bfe1b2c34f1ed9536a516c5d5dd304
Nick Booker <nick.booker@opusvl.com>
2020-07-21 15:10:39 +0100

    Replace unnecessary public attribute with generic function [REF]
    The main_account_id is now computed directly from account_ids, but using
    helper function first_or_null()

--- om_account_budget/models/account_budget.py
+++ om_account_budget/models/account_budget.py
@@ -17,13 +17,9 @@ class AccountBudgetPost(models.Model):
         domain=[('deprecated', '=', False)])
     company_id = fields.Many2one('res.company', 'Company', required=True,
         default=lambda self: self.env['res.company']._company_default_get('account.budget.post'))
-    first_account_id = fields.Many2one(
-        comodel_name='account.account',
-        compute='_compute_first_account_id',
-    )
     main_account_id = fields.Many2one(
         comodel_name='account.account',
-        related='first_account_id',
+        compute='_compute_main_account_id',
         help='The account that related objects can use when they need a singleton',
     )
 
@@ -38,13 +34,10 @@ class AccountBudgetPost(models.Model):
             raise ValidationError(_('The budget must have at least one account.'))
 
     @api.depends('account_ids')
-    def _compute_first_account_id(self):
+    def _compute_main_account_id(self):
         for budgetary_position in self:
-            budgetary_position.first_account_id = (
-                budgetary_position.account_ids[0]
-                if budgetary_position.account_ids
-                else null_object(budgetary_position.account_ids)
-            )
+            budgetary_position.main_account_id = \
+                first_or_null(budgetary_position.account_ids)
 
     @api.model
     def create(self, vals):
@@ -309,6 +302,15 @@ class CrossoveredBudgetLines(models.Model):
                     raise ValidationError(_(
                         '"End Date" of the budget line should be included in '
                         'the Period of the budget'))
+
+
+# TODO belongs in a library
+def first_or_null(recset):
+    """Return the first of the recordset, or the null object of same type if empty"""
+    if recset:
+        return recset[0]
+    else:
+        return null_object(recset)
 
 
 # TODO belongs in a library
