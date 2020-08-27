6f31921c4e51ad0dbd8f824d16102bfac1a394e0
Armand Cela <armand.cela@opusvl.com>
2020-08-27 10:21:23 +0100

    [O2593] Warning or blocking messages on payments

--- approval_hierarchy/models/account_payment.py
+++ approval_hierarchy/models/account_payment.py
@@ -49,7 +49,9 @@ class AccountPayment(models.Model):
         if not self.partner_id or not self.env.user.has_group(
                 'approval_hierarchy.group_warning_payment'):
             return {}
-        if self._context.get('active_model') and self._context.get('active_ids'):
+        if self._context.get('active_model') and self._context.get(
+                'active_ids') and \
+                self._context.get('active_model') != 'account.payment.register':
             return {}
         warning = {}
         title = False
@@ -101,3 +103,45 @@ class AccountPayment(models.Model):
             return {'warning': warning}
         return {}
 
+
+class PaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    def _prepare_payment_vals(self, invoices):
+        payment_vals = super(PaymentRegister, self)._prepare_payment_vals(
+            invoices)
+        partner = invoices[0].commercial_partner_id
+        # If partner has no warning, check its company
+        if partner.payment_warn == 'no-message' and partner.parent_id:
+            partner = partner.parent_id
+        if partner.payment_warn and partner.payment_warn != 'no-message':
+            payment_vals.update({'partner_id': False})
+        return payment_vals
+
+    # Override function to post only the payments that has filled the partner
+    def create_payments(self):
+        '''Create payments according to the invoices.
+        Having invoices with different commercial_partner_id or different type
+        (Vendor bills with customer invoices) leads to multiple payments.
+        In case of all the invoices are related to the same
+        commercial_partner_id and have the same type, only one payment will be
+        created.
+
+        :return: The ir.actions.act_window to show created payments.
+        '''
+        Payment = self.env['account.payment']
+        payments = Payment.create(self.get_payments_vals())
+        payments.filtered(lambda payment: payment.partner_id).post()
+
+        action_vals = {
+            'name': _('Payments'),
+            'domain': [('id', 'in', payments.ids)],
+            'res_model': 'account.payment',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+        }
+        if len(payments) == 1:
+            action_vals.update({'res_id': payments[0].id, 'view_mode': 'form'})
+        else:
+            action_vals['view_mode'] = 'tree,form'
+        return action_vals
