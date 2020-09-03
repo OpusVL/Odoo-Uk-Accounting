81b2dc822cf2cd1de7394497f6d31d3614e486b2
Armand Cela <armand.cela@opusvl.com>
2020-09-03 14:42:41 +0100

    [O2593] explaining the reason we need to keep the changes for reflect_field_params

--- approval_hierarchy/models/ir_model_fields.py
+++ approval_hierarchy/models/ir_model_fields.py
@@ -8,9 +8,13 @@ class IrModelField(models.Model):
     _inherit = 'ir.model.fields'
 
     def _reflect_field_params(self, field):
-        """ Tracking value can be either a boolean enabling tracking mechanism
-        on field, either an integer giving the sequence. Default sequence is
-        set to 100. """
+        """Setting all the fields from a list in helpers.py to the model
+        ir.model.fields tracking True. This function is not enough to create
+        logs for the fields to be tracked. For that is inherited
+        _get_tracked_fields in mail_thread.py. We are keeping this functionality
+        to have consistency in the fields model, so they will be tracking True
+        on ir model.fields
+        """
         vals = super(IrModelField, self)._reflect_field_params(field)
         model = self.env['ir.model']._get(field.model_name)
         fields_to_be_tracked = helpers.get_fields_to_be_tracked()
