5b8a9dd99960d6a3f7f99cdbd3e259f2934c2617
Armand Cela <armand.cela@opusvl.com>
2020-09-17 09:48:16 +0100

    [O2593] Adding Sort Code required, adding it on tree views

--- uk_accounting/models/res_partner.py
+++ uk_accounting/models/res_partner.py
@@ -6,5 +6,5 @@ from odoo import models, fields
 class ResPartnerBank(models.Model):
     _inherit = 'res.partner.bank'
 
-    sort_code = fields.Char()
+    sort_code = fields.Char(required=True)
     short_name = fields.Char()
