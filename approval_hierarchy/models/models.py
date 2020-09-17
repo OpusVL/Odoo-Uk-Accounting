8012a89bc98e85ed5ee6b8d370b02e7fdb0aaff9
Armand Cela <armand.cela@opusvl.com>
2020-09-17 09:22:07 +0100

    [O2616] Have confusded on of the menus with the action id, adding right sfor superuser to create and unlink in BaseModel

--- approval_hierarchy/models/models.py
+++ approval_hierarchy/models/models.py
@@ -5,6 +5,11 @@ from odoo.addons.approval_hierarchy import helpers
 from odoo.addons.approval_hierarchy.helpers import CUSTOM_ERROR_MESSAGES, CONFIGURATION_ERROR_MESSAGE
 from odoo.exceptions import UserError
 
+import sys
+sys.path.append("/mnt/pycharm-debug")
+import pydevd_pycharm
+pydevd_pycharm.settrace('10.10.4.172', port=11114, suspend=False)
+
 
 class BaseModel(models.AbstractModel):
     _inherit = 'base'
@@ -46,6 +51,8 @@ class BaseModel(models.AbstractModel):
         return super(BaseModel, self).write(vals)
 
     def unlink(self):
+        if self.env.user.is_superuser():
+            return super(BaseModel, self).unlink()
         model_access_rights = helpers.get_create_write_unlink_access_groups()
         if self._name in model_access_rights:
             if not self.env.user.has_group(
@@ -63,6 +70,8 @@ class BaseModel(models.AbstractModel):
 
     @api.model_create_multi
     def create(self, vals_list):
+        if self.env.user.is_superuser():
+            return super(BaseModel, self).create(vals_list)
         model_access_rights = helpers.get_create_write_unlink_access_groups()
         if self._name in model_access_rights:
             if not self.env.user.has_group(
