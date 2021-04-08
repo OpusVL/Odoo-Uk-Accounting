4fac8ebe5a6d3b18a0ee359dacda8e1fdbb1424e
James Curtis <mybastion@gmail.com>
2021-04-08 17:06:01 +0100

    [FIX] Module cannot be used with l10n_uk
    The original design of this module meant that it could not be used at the same time as l10n_uk. Because the 'account' module automatically installs l10n_uk, hacks were required to make it work. This commit refactors the whole module to inherit l10n_uk and update existing records instead of conflicting with them.

--- uk_accounting/__manifest__.py
+++ uk_accounting/__manifest__.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-
-##############################################################################
-#
-# UK Accounting Localization
-# Copyright (C) 2019 Opus Vision Limited (<https://opusvl.com>)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 {
     "name": "UK Accounting Localization",
     "summary": "UK Accounting: Localization",
@@ -33,22 +12,24 @@
     "depends": [
         "account",
         "base_iban",
-        "base_vat" "l10n_uk",
+        "base_vat",
+        "l10n_uk",
     ],
     # always loaded
     "data": [
-        "security/ir.model.access.csv",
+        # Security
         "security/uk_accounting_security.xml",
-        "data/uk_accounting_chart_data.xml",
+
+        # Data
         "data/account_type_data.xml",
-        "data/account_payment_method.xml",
+        "data/l10n_uk_chart_data.xml",
         "data/account.account.template.csv",
         "data/account.chart.template.csv",
-        "data/account.tax.group.csv",
-        "data/account_tax_report_data.xml",
+        "data/account_payment_method.xml",
         "data/account_tax_data.xml",
         "data/account_chart_template_data.xml",
-        "data/ir_sequence_data.xml",
+
+        # Views
         "views/account_account_view.xml",
         "views/account_move_view.xml",
         "views/res_partner_bank_view.xml",
